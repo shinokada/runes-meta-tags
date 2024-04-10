958e81069b84866f3c24512e52cfda49751b6aa8
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-10 21:46:32 +0200

    fix: add optional to twitter? and og?

--- src/lib/RunesMetaTags.svelte
+++ src/lib/RunesMetaTags.svelte
@@ -51,64 +51,64 @@
   {/if}
 
   {#if twitter}
-    {#if twitter.card}
+    {#if twitter?.card}
       <meta name="twitter:card" content={twitter.card} />
     {/if}
 
-    {#if twitter.handle}
+    {#if twitter?.handle}
       <meta name="twitter:creator" content={twitter.handle} />
     {/if}
 
-    {#if twitter.site}
+    {#if twitter?.site}
       <meta name="twitter:title" content={twitter.title} />
     {/if}
 
-    {#if twitter.description}
+    {#if twitter?.description}
       <meta name="twitter:description" content={twitter.description} />
     {/if}
 
-    {#if twitter.image}
+    {#if twitter?.image}
       <meta name="twitter:image" content={twitter.image} />
     {/if}
 
-    {#if twitter.imageAlt}
+    {#if twitter?.imageAlt}
       <meta name="twitter:image:alt" content={twitter.imageAlt} />
     {/if}
   {/if}
   {#if og}
-    {#if og.url}
+    {#if og?.url}
       <meta property="og:url" content={og.url} />
     {/if}
 
-    {#if og.type}
+    {#if og?.type}
       <meta property="og:type" content={og.type} />
     {/if}
 
-    {#if og.title}
+    {#if og?.title}
       <meta property="og:title" content={og.title} />
     {/if}
 
-    {#if og.description}
+    {#if og?.description}
       <meta property="og:description" content={og.description} />
     {/if}
 
-    {#if og.image}
+    {#if og?.image}
       <meta property="og:image" content={og.image} />
     {/if}
 
-    {#if og.imageAlt}
+    {#if og?.imageAlt}
       <meta property="og:image:alt" content={og.imageAlt} />
     {/if}
 
-    {#if og.imageWidth}
+    {#if og?.imageWidth}
       <meta property="og:image:width" content={og.imageWidth} />
     {/if}
 
-    {#if og.imageHeight}
+    {#if og?.imageHeight}
       <meta property="og:image:height" content={og.imageHeight} />
     {/if}
 
-    {#if og.siteName}
+    {#if og?.siteName}
       <meta property="og:site_name" content={og.siteName} />
     {/if}
   {/if}
