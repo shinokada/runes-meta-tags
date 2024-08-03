6e9bf740e3e3365f5237845faf45376a36bb80a4
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-08-03 17:37:58 +0200

    fix: update for svelte-5-ui-lib and runes-webkit

--- src/routes/utils/Nav.svelte
+++ src/routes/utils/Nav.svelte
@@ -72,7 +72,7 @@
   });
 </script>
 
-{#snippet navLi(lis)}
+{#snippet navLi(lis: LiType[])}
   {#each lis as { name, href, icon }}
     {#if icon}
       <svelte:component this={icon} class="mb-3 h-8 w-8 {random_tailwind_color()}" />
