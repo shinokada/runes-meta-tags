53725219f83d92799e038a694a31f11ae6ac91cd
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-08-25 09:25:58 +0200

    fix: svelte-5-ui-lib runes-webkit update

--- src/routes/utils/Nav.svelte
+++ src/routes/utils/Nav.svelte
@@ -9,7 +9,7 @@
     uiHelpers,
     Darkmode,
     Dropdown,
-    DropdownItem
+    DropdownUl, DropdownLi
   } from 'svelte-5-ui-lib';
   import { page } from '$app/stores';
   import { GithubSolid, random_tailwind_color, DotsHorizontalOutline, XSolid } from 'runes-webkit';
@@ -95,22 +95,23 @@
           <Dropdown
             {dropdownStatus}
             {closeDropdown}
-            {transitionParams}
-            divClass="absolute -left-[50px] top-8 w-12 pl-1.5"
-          >
+            params={transitionParams}
+            class="absolute -left-[50px] top-2 w-12 pl-1.5">
+          <DropdownUl>
             {#if twitterUrl}
-              <DropdownItem href={twitterUrl} target="_blank" aClass="p-2 m-0"
-                ><XSolid /></DropdownItem
+              <DropdownLi href={twitterUrl} target="_blank" aClass="p-2 m-0"
+                ><XSolid /></DropdownLi
               >
             {/if}
             {#if githubUrl}
-              <DropdownItem href={githubUrl} target="_blank" aClass="p-2 m-0">
+              <DropdownLi href={githubUrl} target="_blank" aClass="p-2 m-0">
                 <GithubSolid />
-              </DropdownItem>
+              </DropdownLi>
             {/if}
-            <DropdownItem>
+            <DropdownLi>
               <Darkmode class="p-2 m-0" />
-            </DropdownItem>
+            </DropdownLi>
+          </DropdownUl>
           </Dropdown>
         </div>
       </div>
